fbae1dec110e3a999c16a42bdc8b5007969f273d
Ana Carolina Santos <carolanap7@gmail.com>
2021-09-07 17:20:43 -0300

    Atualizando arquivo index

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,13 @@
 const express = require ('express')
 const router = express.Router()
-const controller = require('../controllers/indexController')
 
-
-router.get('/', controller.welcome)
+router.get("/", (req, res) => {
+    res.status(200).send({
+        "projeto": "sempreAjudar",
+        "version": "1.0.0",
+        "message": "O sempreAjudar é um projeto de conclusão de curso do bootcamp de Back-end da [{reprograma}](https://reprograma.com.br/) em parceria com a [Accenture](https://www.accenture.com/br-pt). O projeto consiste na construção de uma API que tem como objetivo realizar doações para diversas ONGs do Brasil."
+    })
+})
 
 
 module.exports = router
